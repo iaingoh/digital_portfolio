d1c2c61095cf3089f6681f43d997a628e7a418bd
Iain Goh <iaingoh@gmail.com>
2019-08-16 12:04:41 -0700

    Added models. Project and subtitle

--- app_digital_portfolio/portfolio/models.py
+++ app_digital_portfolio/portfolio/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 # TODO: create Project model
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    img = models.ImageField(default='default.jpg')
+
+    def __str__(self):
+        return self.title
+
+class ProjectSubtitle(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    # List of bullet points
+    
+    def __str__(self):
+        return self.title
