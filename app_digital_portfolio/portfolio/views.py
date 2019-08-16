03b94ca3af7b6fbee59d37bf91a43b568e9d0b68
Benard Kong <benardkong@hotmail.com>
2019-08-15 22:39:19 -0700

    Add comments for models and views logic

--- app_digital_portfolio/portfolio/views.py
+++ app_digital_portfolio/portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+# TODO: import Portfolio Model / Project model?
 
 # Create your views here.
+def portfolio_view(request):
+    # TODO: query all Project models
+    # eg. projects = Project.objects.all()
+    # TODO: loop through all projects
+    return HttpResponse("Hello World")
