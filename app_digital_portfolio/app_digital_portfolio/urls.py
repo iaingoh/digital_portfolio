9dc3bf6886630f098d7464b3934cf28f120d5b4a
Benard Kong <benardkong@hotmail.com>
2019-08-30 09:38:45 -0700

    Add Django REST framework requirements

--- app_digital_portfolio/app_digital_portfolio/urls.py
+++ app_digital_portfolio/app_digital_portfolio/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.home),
-    path('portfolio/', include('portfolio.urls'))
+    path('portfolio/', include('portfolio.urls')),
+    path(r'^api-auth/', include('rest_framework.urls'))
 ]
