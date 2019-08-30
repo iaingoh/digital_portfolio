eb9c4bfe2a8fadfbd2610e2932a92aac1f074ae5
Iain Goh <iaingoh@gmail.com>
2019-08-30 12:01:12 -0700

    Set up the models

--- app_digital_portfolio/portfolio/admin.py
+++ app_digital_portfolio/portfolio/admin.py
@@ -4,4 +4,6 @@ from .models import Project, ProjectSubtitle, BulletPoint
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
     pass
-admin.site.register(Project, ProjectAdmin, BulletPoint)
+admin.site.register(Project)
+admin.site.register(ProjectSubtitle)
+admin.site.register(BulletPoint)
